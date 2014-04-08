35438367c80abdde19ee19cb25970af6ae6d29e0
vivekatclg <vivekatclg@gmail.com>
2014-04-08 03:04:09 -0400

    This is very new and latest Project

---
+++ FirstJenkinsCIProj/src/main/java/com/vivek/swe437/FirstJenkinsCIProj/App.java
@@ -0,0 +1,13 @@
+package com.vivek.swe437.FirstJenkinsCIProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
